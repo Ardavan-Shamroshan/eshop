d3e72508be27f8456506a8db88183a0d75620a2a
Ardavan-Shamroshan <ardavan2000.ashr@gmail.com>
2022-11-03 23:24:14 +0330

    django admin, admin.ModelAdmin representation of a model in the admin interface started

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
 
-admin.site.register(models.Product)
+# The ModelAdmin class is the representation of a model in the admin interface.
+# Usually, these are stored in a file named admin.py in your application.
+class ProductAdmin(admin.ModelAdmin):
+    # readonly_fields = ['slug']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    list_display = [
+        'title',
+        'price',
+        'summary',
+        'rating',
+        'is_active',
+    ]
+    list_filter = [
+        'rating',
+        'is_active'
+    ]
+    list_editable = [
+        'is_active'
+    ]
+
+
+admin.site.register(models.Product, ProductAdmin)
