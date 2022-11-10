c7098046b54f27cb8c3864d2742899a3638201ca
Ardavan-Shamroshan <ardavan2000.ashr@gmail.com>
2022-11-11 00:03:45 +0330

    templates statics made dynamic

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def contactUs(request):
+    return render(request, 'home/contact-us.html')
